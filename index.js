5d42f9889a1518132070a8176c529368cadd6c67
Júlio Campos <jcserracampos@gmail.com>
2018-06-02 20:59:16 -0300

    A running bot is a beatiful bot

---
+++ index.js
@@ -0,0 +1,71 @@
+var Twit = require('twit');
+
+require('dotenv').config();
+
+/* Configure the Twitter API */
+const Bot = new Twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+  timeout_ms:           60 * 1000,
+});
+
+var TWITTER_SEARCH_PHRASE = '#100diasdecodigo';
+
+console.log('The bot is running...');
+
+/* BotInit() : To initiate the bot */
+function BotInit() {
+	Bot.post('statuses/retweet/:id', { id: '1003058224348704770' }, BotInitiated);
+	
+	function BotInitiated (error, data, response) {
+		if (error) {
+			console.log('Bot could not be initiated, : ' + error);
+		}
+		else {
+  			console.log('Bot initiated : 1003058224348704770');
+		}
+	}
+	
+	BotRetweet();
+}
+
+/* BotRetweet() : To retweet the matching recent tweet */
+function BotRetweet() {
+
+	var query = {
+		q: TWITTER_SEARCH_PHRASE,
+		result_type: "recent"
+	}
+
+	Bot.get('search/tweets', query, BotGotLatestTweet);
+
+	function BotGotLatestTweet (error, data, response) {
+		if (error) {
+			console.log('Bot could not find latest tweet, : ' + error);
+		}
+		else {
+			var id = {
+				id : data.statuses[0].id_str
+			}
+
+			Bot.post('statuses/retweet/:id', id, BotRetweeted);
+			
+			function BotRetweeted(error, response) {
+				if (error) {
+					console.log('Bot could not retweet, : ' + error);
+				}
+				else {
+					console.log('Bot retweeted : ' + id.id);
+				}
+			}
+		}
+	}
+}
+
+/* Set an interval of 30 minutes (in microsecondes) */
+setInterval(BotRetweet, 30*60*1000);
+
+/* Initiate the Bot */
+BotInit();

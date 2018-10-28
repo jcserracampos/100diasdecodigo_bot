aa499b383cc077a9c16db8c6c5994d3c5bc6b097
Warley Franco <warleypsf@gmail.com>
2018-10-27 23:55:00 -0300

    Change code for tests and adding smoke tests

--- index.spec.js
+++ index.spec.js
@@ -1,3 +1,45 @@
+const TwitterBot = require('./index.js');
+const Twit = require('twit');
+
 test('Only a mock test', () => {
     expect(true).toBe(true);
 });
+
+
+// Smoke Test
+describe('Smoke Tests', () => {
+    describe('Bot', () => {
+        it('should exists as object', () => {
+            expect(typeof TwitterBot.Bot).toBe('object');
+        });
+
+        it('should be instance of Twit', () => {
+            expect(TwitterBot.Bot).toBeInstanceOf(Twit);
+        });
+
+        it('should Bot config have twitter access properties', () => {
+            const expected = {
+                consumer_key: expect.anything(),
+                consumer_secret: expect.anything(),
+                access_token: expect.anything(),
+                access_token_secret: expect.anything(),
+            };
+            expect(TwitterBot.Bot.config).toEqual(expect.objectContaining(expected))
+        });
+
+    });
+
+    describe('BotRetweet', () => {
+        it('should BotRetweet exist and be a function', () => {
+            expect(typeof TwitterBot.BotRetweet).toBe('function');
+        });
+    });
+
+    describe('isReply', () => {
+        it('should exist and be a function', () => {
+            expect(typeof TwitterBot.isReply).toBe('function');
+        });
+    });
+
+})
+
